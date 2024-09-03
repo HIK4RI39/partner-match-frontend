83aac1b0732531dc389f613f5b1c16763d3887ee
HIK4RI39 <14401191+hik4ri39@user.noreply.gitee.com>
2024-09-03 17:26:25 +0800

    分页查询首页推荐用户

--- src/components/user-card-list.vue
+++ src/components/user-card-list.vue
@@ -1,19 +1,21 @@
 <template>
-  <van-card
-      v-for="user in userList"
-      :title="`${user.username} (${user.userAccount})`"
-      :thumb="user.avatarUrl"
-      :tag="user.id"
-  >
-    <template #tags>
-      <van-tag plain type="danger" v-for="tag in user.tags" style="margin-right: 8px">
-        {{ tag }}
-      </van-tag>
-    </template>
-    <template #footer>
-      <van-button size="mini">联系我</van-button>
-    </template>
-  </van-card>
+  <div id="content">
+    <van-card
+        v-for="user in userList"
+        :title="`${user.username} (${user.userAccount})`"
+        :thumb="user.avatarUrl"
+        :tag="user.id"
+    >
+      <template #tags>
+        <van-tag plain type="danger" v-for="tag in user.tags" style="margin-right: 8px">
+          {{ tag }}
+        </van-tag>
+      </template>
+      <template #footer>
+        <van-button size="mini">联系我</van-button>
+      </template>
+    </van-card>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -29,5 +31,7 @@ const props = withDefaults(defineProps<UserCardListProps>(), {
 </script>
 
 <style scoped>
-
+#content {
+  padding-bottom: 100px;
+}
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,88 @@
 <script setup lang="ts">
+import {onMounted, Ref, ref} from "vue";
+import myAxios from "../plugins/myAxios.ts";
+import {showToast} from "vant";
+import {UserType} from "../models/user";
+import UserCardList from "../components/user-card-list.vue";
+
+const userList:Ref<UserType[]> = ref([]);
+
+const currentPage = ref(  1);
+const total = ref(50);
+const pageSize = ref(10);
+
+const doResearch = async () => {
+  const userListData = await myAxios.get(
+      '/user/recommend', {
+        params:{
+          currentPage: currentPage.value,
+          total: total.value,
+          pageSize: pageSize.value
+        }
+      }
+  ).then(function (response) {
+    console.log('/user/search/tags succeed', response);
+    showToast('请求成功');
+    return response?.data;
+  })
+      .catch(function (error) {
+        console.log('/user/search/tags error', error);
+        showToast('请求失败');
+      });
+
+  if (userListData) {
+    userListData.list.forEach((user:UserType) => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags);
+      }
+    })
+    console.log("userListData: ",userListData);
+    userList.value = userListData.list;
+    total.value = userListData.total;
+  }
+}
 
 
+onMounted(async () => {
+  await doResearch();
+});
 
 </script>
 
 <template>
-主页
+<!--  :desc="user.profile"-->
+  <div id="content">
+    <user-card-list
+        :user-list="userList"
+    />
+    <van-empty v-if="!userList || userList.length<1" description="搜索结果为空"/>
+
+    <van-pagination
+        v-model="currentPage"
+        :total-items="total"
+        :show-page-size="pageSize"
+        :page-count="Math.ceil(total/pageSize)"
+        @change="doResearch"
+        force-ellipses
+        style="
+          position: fixed; bottom: 0;
+          padding: 50px;
+          width: 100%;
+          text-align: center;"
+    >
+      <template #prev-text>
+        <van-icon name="arrow-left" />
+      </template>
+
+      <template #next-text>
+        <van-icon name="arrow" />
+      </template>
+
+      <template #page="{ text }">{{ text }}</template>
+    </van-pagination>
+  </div>
+
+
 </template>
 
 <style scoped>
