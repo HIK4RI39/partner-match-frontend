c7421f0b7067f134a82b18feb34835ef1751ef16
HIK4RI39 <14401191+hik4ri39@user.noreply.gitee.com>
2024-09-04 10:06:21 +0800

    用户头像上传

--- src/pages/User.vue
+++ src/pages/User.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
     <van-cell title="昵称" is-link to="/user/edit" :value="user?.username" @click="toEdit('username', '昵称', user.username)"/>
-    <van-cell title="头像">
-      <img style="height: 48px; width: 48px" :src="user?.avatarUrl">
+
+
+    <van-cell title="头像" @click="toAvatar(user?.avatarUrl)">
+      <img style="height: 72px; width: 72px" :src="user?.avatarUrl">
     </van-cell>
+
     <van-cell title="账号"  :value="user?.userAccount" />
     <van-cell title="性别" is-link to="/user/edit" :value="user?.gender" @click="toEdit('gender', '性别', user.gender)" />
     <van-cell title="电话" is-link to="/user/edit" :value="user?.phone" @click="toEdit('phone', '电话', user.phone)" />
     <van-cell title="邮箱" is-link to="/user/edit" :value="user?.email" @click="toEdit('email', '邮箱', user.email)"/>
     <van-cell title="注册时间" :value="user?.createTime.toString()" />
+  </div>
+
+  <div style="margin: 16px;">
+    <van-button round block type="danger" @click="doLogout">
+      退出
+    </van-button>
   </div>
 
 </template>
@@ -18,15 +27,27 @@ import {onMounted, ref} from "vue";
 import {showToast} from "vant";
 import {GetCurrentUser} from "../services/user.ts";
 import {useRouter} from 'vue-router'
+import myAxios from "../plugins/myAxios.ts";
+import {BaseResponse} from "../models/baseResponse";
 
 const router = useRouter()
 const user = ref();
+
+const doLogout = async () => {
+  await myAxios.post("/user/logout").then((res:BaseResponse)=>{
+    if(res.code===0){
+      showToast("退出登录");
+      setTimeout(() => {
+        location.reload();
+      }, 1000);
+    }
+  })
+}
 
 onMounted(async () => {
   const res = await GetCurrentUser();
   if(res){
     user.value = res;
-    showToast("获取用户信息成功");
   }else{
     showToast("用户未登录");
     // //跳转登录页
@@ -45,7 +66,15 @@ const toEdit = (editKey: string, editName: string, currentValue: any) => {
       currentValue: currentValue
     }
   })
+}
 
+const toAvatar = (currentValue:string) => {
+  router.push({
+    path: '/user/avatar',
+    query: {
+      avatarUrl: currentValue,
+    }
+  })
 }
 </script>
 
---
+++ src/pages/UserAvatar.vue
@@ -0,0 +1,100 @@
+<template>
+  <van-cell title="头像">
+    <van-uploader
+        v-model="fileList"
+        multiple :max-size="500 * 1024"
+        @oversize="onOversize"
+        reupload max-count="1"
+        :after-read="afterRead"
+        :before-read="beforeRead"
+    />
+  </van-cell>
+
+      <van-button round block type="primary" @click="updateAvatar(url)">
+        提交
+      </van-button>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+import {useRoute, useRouter} from "vue-router";
+import {showToast} from "vant";
+import myAxios from "../plugins/myAxios.ts";
+import {BaseResponse} from "../models/baseResponse";
+import {updateCurrentUser} from "../states/user.ts";
+const route = useRoute();
+const router = useRouter();
+
+const avatarUrl = ref(route.query.avatarUrl);
+const forms = new FormData();
+var url = ref('');
+
+const fileList = ref([
+  {
+    url: avatarUrl,
+    deletable: true,
+  }
+]);
+
+const onOversize = (file) => {
+  console.log(file);
+  showToast('文件大小不能超过 500kb');
+};
+
+const beforeRead = (file) => {
+    if (file.type !== 'image/jpeg' && file.type !== 'image/jpg' && file.type !== 'image/png') {
+      showToast('仅限上传jpeg/jpg/png格式文件');
+      return false;
+    }
+    return true;
+}
+
+const afterRead = async (file) => {
+  file.status = 'uploading';
+  file.message = '上传中...';
+  //与后端字段名相同
+  forms.append("image",file.file);
+
+  file.status = 'done';
+  await myAxios.post("/upload", forms).then((res:BaseResponse) => {
+    if(res?.code===0){
+      console.log(res)
+      file.status = 'done';
+      url = res.data;
+    }else{
+      file.status = 'failed';
+      file.message = '上传失败';
+    }
+  }).catch(() => {
+    file.status = 'failed';
+    file.message = '上传失败';
+  })
+};
+
+const updateAvatar = async (avatarUrl:string) => {
+  if(avatarUrl===''){
+    showToast('头像未更新!');
+    return;
+  }
+
+  await myAxios.post(
+    "/user/update/info",
+      JSON.stringify({"avatarUrl": avatarUrl}),
+      {
+        headers: {'Content-Type':'application/json' }
+      }).then((res:BaseResponse) => {
+        if(res?.code===0){
+          updateCurrentUser('avatarUrl', avatarUrl);
+          setTimeout(() => {
+            router.push("/user");
+          }, 1200);
+        }
+  })
+}
+
+</script>
+
+
+<style>
+
+</style>
